dd0536d3ee95769a820e6cf66fc91ee088ee0e01
Wade Urry <wade@iwader.co.uk>
2018-08-13 17:06:00 +0100

    Add API

---
+++ src/RS3/API/API.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace RuneStat\RS3\API;
+
+use Exception;
+use GuzzleHttp\Exception\RequestException;
+use RuneStat\Exceptions\PlayerNotFound;
+use RuneStat\RS3\Activities\Repository as Activities;
+use RuneStat\RS3\Profile;
+use RuneStat\RS3\Stats\Repository as Stats;
+
+class API
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    private $endpoints = [
+        'profile' => 'https://apps.runescape.com/runemetrics/profile/profile?user=%s&activities=20',
+    ];
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    protected function getEndpoint(string $endpoint, ...$params)
+    {
+        if (! array_key_exists($endpoint, $this->endpoints)) {
+            throw new Exception(
+                sprintf('Unknown endpoint [%s] requested', $endpoint)
+            );
+        }
+
+        return sprintf($this->endpoints[$endpoint], ...$params);
+    }
+
+    /**
+     * @param string $rsn
+     * @return Profile
+     * @throws PlayerNotFound
+     */
+    public function getProfile(string $rsn): Profile
+    {
+        try {
+            $response = $this->client->get(sprintf($this->endpoints['profile'], $rsn));
+        } catch (RequestException $e) {
+            $response = $e->getResponse();
+
+            if (! is_null($response) && $response->getStatusCode() === 404) {
+                throw new PlayerNotFound($rsn);
+            }
+
+            throw $e;
+        }
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        $stats = Stats::fromProfileJson($json['skillvalues']);
+        $activities = Activities::fromProfileJson($json['activities']);
+
+        return new Profile($stats, $activities);
+    }
+}
