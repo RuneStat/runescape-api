0103ad6648914858c45ec3f3520f07df05a1b9b4
Wade Urry <wade@iwader.co.uk>
2018-08-29 20:48:09 +0100

    Check for spaces in the validate_rsn regex

--- src/functions.php
+++ src/functions.php
@@ -7,7 +7,7 @@ namespace RuneStat;
 if (! function_exists('\RuneStat\validate_rsn')) {
     function validate_rsn(string $rsn): bool
     {
-        return (bool) preg_match('/^[a-z0-9\-_]{1,12}$/i', $rsn);
+        return (bool) preg_match('/^[a-z0-9\-_ ]{1,12}$/i', $rsn);
     }
 }
 
