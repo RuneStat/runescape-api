432c40de4754c88665aa0f126c9c9eb0fe4b9f74
Wade Urry <wade@iwader.co.uk>
2018-12-17 15:06:10 +0100

    Add Profile::getCombatLevel() method

--- src/RS3/Profile.php
+++ src/RS3/Profile.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace RuneStat\RS3;
 
+use RuneStat\RS3\combat_level;
 use RuneStat\RS3\Activities\Repository as Activities;
 use RuneStat\RS3\Stats\Repository as Stats;
+use RuneStat\RS3\Skills\Attack;
+use RuneStat\RS3\Skills\Strength;
+use RuneStat\RS3\Skills\Defence;
+use RuneStat\RS3\Skills\Magic;
+use RuneStat\RS3\Skills\Prayer;
+use RuneStat\RS3\Skills\Ranged;
+use RuneStat\RS3\Skills\Summoning;
+use RuneStat\RS3\Skills\Constituion;
 
 class Profile
 {
@@ -44,5 +53,19 @@ class Profile
     public function getActivities(): Activities
     {
         return $this->activities;
+    }
+
+    public function getCombatLevel(): int
+    {
+        return combat_level(
+            $this->stats->findByClass(Attack::class),
+            $this->stats->findByClass(Strength::class),
+            $this->stats->findByClass(Magic::class),
+            $this->stats->findByClass(Ranged::class),
+            $this->stats->findByClass(Defence::class),
+            $this->stats->findByClass(Constituion::class),
+            $this->stats->findByClass(Prayer::class),
+            $this->stats->findByClass(Summoning::class),
+        );
     }
 }
